fb44f3c5ea322e4a86a88b38d8def1238649f021
gtverse00 <120357867+gtverse00@users.noreply.github.com>
2026-06-10 00:13:24 +0800

    Add server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
